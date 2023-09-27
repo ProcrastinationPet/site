d4263111c8b2f7d0755c3ed4a7aa4a83b7e61b27
AuTasks <129902130+autasks@users.noreply.github.com>
2023-09-27 00:36:29 -0300

    Finalização da Responsividade

--- js/script.js
+++ js/script.js
@@ -4,8 +4,8 @@ const buttons = document.querySelectorAll('.button');
 buttons.forEach(button => {
     button.addEventListener('click', () => {
         const a = document.createElement('a');
-        a.href = 'file/apk.apk';
-        a.download = 'apk.apk';
+        a.href = '../file/AuTasks-v0.2.0-alpha.apk';
+        a.download = 'AuTasks-v0.2.0-alpha.apk';
         a.click();
     })
 });
