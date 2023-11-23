e5753f86b93903114b7ca8efa99293de6786a4b5
AuTasks <129902130+autasks@users.noreply.github.com>
2023-11-23 09:10:07 -0300

    Update script.js

--- js/script.js
+++ js/script.js
@@ -5,7 +5,7 @@ buttons.forEach(button => {
     button.addEventListener('click', () => {
         const a = document.createElement('a');
         a.href = 'https://drive.google.com/file/d/11Aryk4odUmL7dwkSwKy1Nfmw7u5ytse0/view?usp=sharing';
-        a.download = 'AuTasks-v0.3.0-alpha.apk';
+        a.download = 'AuTasks-v0.9.2.apk';
         a.click();
     })
 });
